d70f9f7169f73a868e4a438363f39e416489f637
Sabbir Ahmed <mdsabbir5820@gmail.com>
2025-11-12 22:21:25 +0600

    fixed allowed host

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-1er-or_bfc*d_q6jlr$f6ah!#9wvj3x9@r##yjswzl6(t9!p!-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
